afb8548545267a68bc9ce61ff857ddeb65ea25a2
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-04-22 16:22:00 +0200

    fix: remove unsused lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from "react-dom";
 import "@gouvfr/dsfr/dist/dsfr/dsfr.min.css";
-import { App } from "./App";
-import { kcContext } from "./KcApp/kcContext";
+import { kcContext } from "./kcContext";
 import { KcApp } from "./KcApp";
 
 ReactDOM.render(
-  kcContext === undefined ? <App /> : <KcApp kcContext={kcContext} />,
+  kcContext === undefined ? <div /> : <KcApp kcContext={kcContext} />,
   document.getElementById("root")
 );
