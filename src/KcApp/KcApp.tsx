7c4fd5bd23890ee466b0f4cb677ba640b5476d56
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-04-22 15:50:21 +0200

    feat: add theme

--- src/KcApp/KcApp.tsx
+++ src/KcApp/KcApp.tsx
@@ -1,13 +1,11 @@
 import { memo } from "react";
 import type { KcContext } from "./kcContext";
 import { defaultKcProps as props } from "keycloakify";
-import { Login } from "keycloakify/lib/components/Login";
+import { Login } from "./Login";
 import { Register } from "./Register";
 import { Info } from "keycloakify/lib/components/Info";
 import { Error } from "keycloakify/lib/components/Error";
 import { Terms } from "./Terms";
-import { MyExtraPage1 } from "./MyExtraPage1";
-import { MyExtraPage2 } from "./MyExtraPage2";
 import { KcApp as KcAppBase } from "keycloakify/lib/components/KcApp";
 import "./kcMessagesExtension";
 
@@ -18,8 +16,6 @@ export const KcApp = memo(({ kcContext }: { kcContext: KcContext; }) => {
         case "info.ftl": return <Info {...{ kcContext, ...props }} />;
         case "error.ftl": return <Error {...{ kcContext, ...props }} />;
         case "terms.ftl": return <Terms {...{ kcContext, ...props }} />;
-        case "my-extra-page-1.ftl": return <MyExtraPage1 {...{ kcContext, ...props }} />;
-        case "my-extra-page-2.ftl": return <MyExtraPage2 {...{ kcContext, ...props }} />;
         default: return <KcAppBase {...{ kcContext, ...props }} />;
     }
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from "react-dom";
+import "@gouvfr/dsfr/dist/dsfr/dsfr.min.css";
 import { App } from "./App";
 import { kcContext } from "./KcApp/kcContext";
 import { KcApp } from "./KcApp";
 
 ReactDOM.render(
-  kcContext === undefined ?
-    <App /> :
-    <KcApp kcContext={kcContext} />,
+  kcContext === undefined ? <App /> : <KcApp kcContext={kcContext} />,
   document.getElementById("root")
 );
