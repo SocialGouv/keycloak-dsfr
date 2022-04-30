cdcf9e8db9e7320dfe6efafa46f2da344dab3980
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-04-30 17:32:23 +0200

    build

--- src/Template.tsx
+++ src/Template.tsx
@@ -13,8 +13,92 @@ import {
   getBestMatchAmongKcLanguageTag,
   getKcLanguageTagLabel,
 } from "keycloakify";
-import { pathJoin } from "keycloakify/lib/tools/pathJoin.js";
-import { headInsert } from "keycloakify/lib/tools/headInsert.js";
+import { Deferred } from "evt/tools/Deferred";
+
+if (!HTMLElement.prototype.prepend) {
+  HTMLElement.prototype.prepend = function (childNode) {
+    if (typeof childNode === "string") {
+      throw new Error("Error with HTMLElement.prototype.appendFirst polyfill");
+    }
+
+    this.insertBefore(childNode, this.firstChild);
+  };
+}
+
+function pathJoin(...path: string[]): string {
+  return path
+    .map((part, i) => (i === 0 ? part : part.replace(/^\/+/, "")))
+    .map((part, i) => (i === path.length - 1 ? part : part.replace(/\/+$/, "")))
+    .join("/");
+}
+
+function headInsert(
+  params:
+    | {
+        type: "css";
+        href: string;
+        position: "append" | "prepend";
+      }
+    | {
+        type: "javascript";
+        src: string;
+      }
+) {
+  const htmlElement = document.createElement(
+    (() => {
+      switch (params.type) {
+        case "css":
+          return "link";
+        case "javascript":
+          return "script";
+      }
+    })()
+  );
+
+  const dLoaded = new Deferred<void>();
+
+  htmlElement.addEventListener("load", () => dLoaded.resolve());
+
+  Object.assign(
+    htmlElement,
+    (() => {
+      switch (params.type) {
+        case "css":
+          return {
+            href: params.href,
+            type: "text/css",
+            rel: "stylesheet",
+            media: "screen,print",
+          };
+        case "javascript":
+          return {
+            src: params.src,
+            type: "text/javascript",
+          };
+      }
+    })()
+  );
+
+  document.getElementsByTagName("head")[0][
+    (() => {
+      switch (params.type) {
+        case "javascript":
+          return "appendChild";
+        case "css":
+          return (() => {
+            switch (params.position) {
+              case "append":
+                return "appendChild";
+              case "prepend":
+                return "prepend";
+            }
+          })();
+      }
+    })()
+  ](htmlElement);
+
+  return dLoaded.pr;
+}
 
 export type TemplateProps = {
   displayInfo?: boolean;
